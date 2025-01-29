983d73b7ebc7023badf117e11fe9804b306f4db3
SuperShrew <150274640+SuperShrew@users.noreply.github.com>
2025-01-29 14:28:06 +0000

    messin

--- code1.cpp
+++ code1.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string idk(string in) {
+  in = in + " idk";
+  return in;
+}
+
 int main() {
+  string a;
   cout << "hi" << endl;
+  cin >> a;
+  cout << a << " hi" << endl;
+  cout << idk(a) << endl;
   return 0;
 }
