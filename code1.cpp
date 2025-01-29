b3f5483bc2eb461af2cc96e611050a938a16aa41
SuperShrew <150274640+SuperShrew@users.noreply.github.com>
2025-01-29 15:10:23 +0000

    MOAR

--- code1.cpp
+++ code1.cpp
@@ -10,9 +10,17 @@ string idk(string in) {
 
 int main() {
   string a;
+  const double pi = 3.14159;
   cout << "hi" << endl;
   cin >> a;
   cout << a << " hi" << endl;
   cout << idk(a) << endl;
+  cout << 2+2 << endl;
+  cout << "pi is " << pi << " to 5.d.p." << endl;
+  if (a == "eieio") {
+    cout << "ON THAT fARM HE HAD SOME RAM" << endl;
+    cout << "eieio" << endl;
+    cout << "with data here and data there" << endl;
+  }
   return 0;
 }
